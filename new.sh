02f949528514a57b3a1389af34b0d0930ece7378
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-08-19 13:52:01 +0530

    Thing

--- new.sh
+++ new.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+echo "This is mE"
 
-if [ "$name" == "mE" ]; then
-	echo "mE, Welcome"
+#name="mE"
+#echo "my $name"
+
+if [[ $name == "Bikku" ]]; then
+    echo "Welcome, Bikku!"
 else
-	echo "who are you?"
+    echo "Who are you?"
 fi
+
+
+#loop
+for i in 1 2 3
+do
+	echo "Number: $i"
+done
