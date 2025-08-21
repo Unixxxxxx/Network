8685cc6b874412c117310ab301ddef6249cd9591
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-08-21 12:32:27 +0530

    save natwork info

--- save.sh
+++ save.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo '== IP Info ==' > Network_report.txt
+echo '=== IP Info ===' > Network_report.txt
 ip a >> Network_report.txt
 
+echo '=== Routes === ' >> Network_report.txt
+ip route >> Network_report.txt
+echo "=== Open Ports ===" >> Network_report.txt
+
+ss -tulnp >> Network_report.txt
+echo "Report save to network.txt"
