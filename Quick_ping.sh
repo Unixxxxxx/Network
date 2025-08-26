a830877119929ffcf61a822f0263d9d0a0ab16f8
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-08-26 13:32:46 +0530

    mof

--- Quick_ping.sh
+++ Quick_ping.sh
@@ -4,5 +4,14 @@ echo "You are"
 whoami 
 
 echo "_____________________________________"
-echo "You are at"
+echo "Print working directory"
 pwd
+
+echo "current data & time"
+date
+
+echo "Today is $(date +%A)"
+echo "========================================"
+echo "Now come to directory"
+mkdir test_dir
+echo "Directory Create"
