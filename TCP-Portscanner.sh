33452ba6ff0c71591dd9e9ef1b4acb46244dfdca
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-08-30 17:04:59 +0530

    del

--- TCP-Portscanner.sh
+++
@@ -1,7 +0,0 @@
-# Usage: ./tcpscan.sh 10.0.0.15 22 80 443 3306
-target="$1"; shift
-for p in "$@"; do
-  timeout 0.5 bash -c "echo >/dev/tcp/$target/$p" 2>/dev/null \
-    && echo "OPEN  - $target:$p" || echo "CLOSED- $target:$p"
-done
-
